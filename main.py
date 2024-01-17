225fed32cff467064e89c7624955d91b165ff090
PyAi.fr <156432911+Alex-PyAi@users.noreply.github.com>
2024-01-17 15:12:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Download series from API 
+# Process user data  
 class Download:
     def __init__(self, user_json):
         """Initialise la classe avec l'URL de l'API."""
@@ -6,14 +6,14 @@ class Download:
 
     def download(self):
         """
-        Permet de télécharger les séries déposé par l'utilisateur.
+        Permet de télécharger les séries déposé par l'utilisateur (excel, csv, json, ...).
         Ajouter des tests pour split ou summarize les données.
         Return JSON format
         """
         # Logique de téléchargement des données de l'utilisateur via drag and drop
         return json_data
 
-# Load JSON inside LLM
+# Load user data inside LLM
 class Train:
     def __init__(self, model):
         """Initialise la classe avec le modèle."""
@@ -26,7 +26,7 @@ class Train:
         # Logique d'ajout des données au modèle ici
         return trained_model
 
-# Chatbox
+# Chat with LLM about your data 
 class Chat:
     def __init__(self, model):
         """Initialise la classe avec le modèle."""
